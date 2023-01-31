3c693840fa5e80c738dc0ba80da0cda9c075bc8b
Pavan Kumar Davuluri <pavan.kumar.davuluri@ibm.com>
2023-01-31 09:38:56 +0530

    Updating the EC2 code

---
+++ 011_ec2.tf
@@ -0,0 +1,43 @@
+locals {
+  ec2_name_1 = "Devops_test_1_EC2"
+}
+
+module "ec2_instance_1" {
+
+  source = "github.com/dkumar-p/terraform-aws-ec2-instance.git?ref=v3.3.0"
+
+  name = local.ec2_name_1
+
+  ami                    = var.ami_master
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  monitoring             = false
+  vpc_security_group_ids = [module.security_group_ec2.security_group_id]
+  subnet_id              = element(module.vpc-1.public_subnets, 0)
+  enable_volume_tags     = false
+
+  root_block_device = [
+    {
+      device_name           = "/dev/sda1"
+      volume_size           = "40"
+      volume_type           = "gp3"
+      encrypted             = true
+      delete_on_termination = false
+      tags = merge(var.tags,
+        {
+          "resource:name" = local.ec2_name_1
+        },
+        {
+          "Name" = local.ec2_name_1
+        }
+      )
+    }
+  ]
+
+  tags = merge(var.tags,
+    {
+      "resource:name" = local.ec2_name_1
+      "Name"          = local.ec2_name_1
+    }
+  )
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,8 +3,16 @@ tags = {
   "environment" = "int"
 }
 
+### VPC Details =========================================
 name_vpc_cluster = "Devops-test-vpc-1"
 vpc_cidr_cluster = "10.98.20.0/25"
 
 vpc_public_subnets_cluster  = ["10.98.20.0/28", "10.98.20.16/28"]
 vpc_private_subnets_cluster = ["10.98.20.32/28", "10.98.20.48/28", "10.98.20.64/28", "10.98.20.80/28"]
+
+
+### EC2 Details =========================================
+
+ami_master    = "ami-01a4f99c4ac11b03c"
+instance_type = "t2.micro"
+key_name      = "devops-test-1"
